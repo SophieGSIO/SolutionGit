90608386fb1d5d656b5b6fa31abc9c082e0e0ab4
SG <sophie.girard@ac-grenoble.fr>
2017-10-27 14:18:19 +0200

    Version initiale

--- ConsoleAppli/Program.cs
+++ ConsoleAppli/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppli
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git !!!");
+            Console.WriteLine("ReHello par SG");
             Console.ReadLine();
         }
     }
